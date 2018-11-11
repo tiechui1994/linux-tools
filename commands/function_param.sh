86e648c3c4140e5670a7782b281f9037bb85be26
tiechui1994 <2904951429@qq.com>
2018-11-11 13:17:15 +0800

    add function @

---
+++ commands/function_param.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#----------------------------------------------------
+# File: function_param.sh
+# Contents: 函数参数
+# Date: 18-11-11
+#----------------------------------------------------
+
+param() {
+    echo "params: $*"
+    echo "params: $@"
+
+    echo "params num: $#"
+    echo "function pid: $$"
+    echo "function return value: $?"
+
+    echo "shell script: $0"
+}
+
+param "AA" "BB"
