0bcfd48f737d49d420555a365c56aa511fd0d48f
tiechui1994 <2904951429@qq.com>
2019-06-05 11:49:18 +0800

    update iptables

--- commands/iptables.sh
+++ commands/iptables.sh
@@ -10,21 +10,23 @@
 #---------------------------------------------------------------------------------------------------
 # description
 #
-# iptables 用于在Linux内核中设置, 维护和检查IPv4数据包过滤规则表. 可以定义几个不同的表, 每个表包含许多内置链. 也可能包
-# 含用户定义的链.
+# iptables 用于在Linux内核中设置, 维护和检查IPv4数据包过滤规则表. 可以定义几个不同的表, 每个表包含许多内置链. 也可能
+# 包含用户定义的链.
 #
-# 每个链是由一组可以匹配数据包的rule组成, 每个rule定义了如何处理匹配的数据包. 这被称为'target', 可以可跳转到同一个表中的
-# 用户定义链.
+# 每个链是由一组可以匹配数据包的rule组成. 每个rule指定应当如何处理与之匹配的packet. 这被称为'target'(目标), 也可以
+# 跳向到同一个表内的用户定义的链.
 #
 #
 # target
 #
-# 防火墙规则指定了packet和target匹配的条件. 如果packet不匹配, 则会使用链中的下一个rule进行匹配. 如果packet匹配, 则下一
-# 条rule由target的值指定, 该值可以是用户定义的链的名称, 也可以是特殊值ACCEPT, DROP, QUEUE 或 RETURN当中之一.
+# 防火墙规则指定了packet和target匹配的条件. 如果packet不匹配, 则会使用链中的下一个rule进行匹配. 如果packet匹配, 则
+# 下一条rule由链的target值指定, 该值可以是用户定义的链名, 也可以是特殊值ACCEPT, DROP, QUEUE 或 RETURN当中之一.
 #
-# ACCEPT表示让packet通过. DROP表示将packet丢弃. QUEUE表示将数据包传递给用户空间. RETURN表示停止遍历当前的链并继续前
-# 一个(calling)链的下一条rule. 如果packet到达内置链的末尾 或者 packet匹配目标RETURN的内置链中的规则, 则链policy指定
-# 的target将决定packet的结果.
+# ACCEPT 表示让packet通过.
+# DROP 表示将packet丢弃.
+# QUEUE 表示将数据包传递给用户空间.
+# RETURN 表示停止当前的链的匹配, 到前一个链的下一条rule重新开始. 如果packet到达内置链的末端或者packet匹配到target是
+# RETURN的内置链中的rule, 则使用链设置的target处理此packet.
 #
 #
 # table
@@ -76,9 +78,79 @@
 #   -N, --new-chain chain
 #
 #   -X, --delete-chain chain
+#   删掉指定的用户自定义的链. 这个链必须没有被引用, 如果被引用, 在删除之前必须删除或替换与之相关的rule.
 #
 #   -E, --rename-chain old-chain new-chain
 #
 #   -P, --policy chain target
+#   设置chain的target. 只有内置(非用户定义)链才能拥有policy, 内置链和用户定义链都不能作为策略的target
+#
+#
+#
+# parameter
+#
+#   -p, --protocol [!] protocol
+#   指定packet的协议. 可选值: tcp, udp, icmp, all, 或者是一个数字. 在/etc/protocols空可以查看到所有支持的协议
+#
+#   -s, --source [!] address[/mask]
+#   指定源地址. address可以是主机名(可以被DNS解析), ip地址(可以带掩码)
+#
+#   -d, --destination [!] address[/mask]
+#   指定目标地址, 同源地址
+#
+#   -j, --jump target
+#   设置rule的target. 可选的值ACCEPT, DROP, QUEUE, RETURN, 或者 用户自定义链名
+#
+#   -g, --goto chain
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #
 #---------------------------------------------------------------------------------------------------
