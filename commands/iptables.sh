31c3b6404a37f2b2d8c813a0d1c0749332ef1829
tiechui1994 <2904951429@qq.com>
2019-06-06 11:49:55 +0800

    add iptables ext match

--- commands/iptables.sh
+++ commands/iptables.sh
@@ -136,45 +136,176 @@
 # account
 #
 # limit
-# 该模块的功能是限制单位时间内进入数据包的数量. 该模块使用token bucket过滤器以有限的速率匹配。 使用此扩展名的规则将匹配,
-# 直到达到此限制（除非使用'！'标志）。
-# 例如，它可以与LOG目标结合使用以提供有限的日志记录.
+# 该模块的功能是限制单位时间内进入数据包的数量. 该模块使用token bucket过滤器限制数据包匹配的速率. 使用此扩展名的规则将
+# 匹配, 直到达到此限制(除非使用'!'标记). 例如, 它可以与 target LOG 组合使用以提供有限的日志记录.
 #
+#   --limit rate
+#   最大平均匹配率: 指定为一个数字, 并带有可选的"/second", "/minute", "/hour" 或"/day"后缀; 默认值为"3/hour".
+#
+#   --limit-burst number
+#   初始化时, 允许最多可以匹配的包, 一旦到达此值, 将使用limit设置的限制. 默认值为5
+#
+#   案例:
+#   iptables -A INPUT -p icmp -m limit --limit 6/minute --limit-burst 5 -j ACCEPT
+#   iptables -A INPUT -p icmp DROP
+#   1分钟之内可以进行6次icmp包匹配 (即每10秒1次). 初始化的时候, 最多允许匹配的icmp数据包是5个. 初始化的icmp累积的包
+#   一旦到达5个之后, 每10秒匹配一次, 没有匹配上的数据包丢弃.
+#
+#
+# mac
+#   --mac-source [!] address
+#   匹配源MAC地址. 它必须是XX:XX:XX:XX:XX:XX的形式. 注意, 这仅适用于来自以太网设备并进入PREROUTING, FORWARD或
+#   INPUT链的数据包.
+#
+#
+# mark
+#   此模块匹配与数据包关联的netfilter标记字段(可使扩展target中的MARK设置)
+#
+#   --mark value[/mask]
+#   匹配具有给定无符号标记值的数据包(如果指定了mask, 则在比较之前与掩码进行逻辑AND运算).
+#
+#
+# multiport
+#   此模块与一组源或目标端口匹配. 最多可指定15个端口. 端口范围(port:port)计为两个端口. 它只能与-p tcp或-p udp一起
+#   使用.
+#
+#   --source-ports [!] port,port:port
+#   如果源端口是给定端口之一, 则匹配. --sports是此选项的别名.
+#
+#   --destination-ports [!] port,port:port
+#   如果目标端口是给定端口之一, 则匹配. --dports是此选项的别名.
+#
+#    --ports  [!] port,port:port
+#   如果源端口或目标端口等于给定端口之一, 则匹配.
+#
+#
+# state
+#   此模块与connection tracking结合使用时, 可以访问到数据包的connection tracking状态。
+#
+#   --state state
+#   其中state是要匹配的连接状态列表. INVALID表示由于某种原因无法识别数据包, 包括内存不足和ICMP错误(与任何已知连接不对
+#   应), ESTABLISHED表示已经建立链接的数据包, NEW表示开始建立新链接的数据包, 与现有的链接没有关联, RELATED表示正在
+#   启动新连接的数据包, 但与现有连接相关联,例如FTP数据传输或ICMP错误.
+#
+#
+# tcp
+#   如果指定了"--protocol tcp", 则会加载这些扩展.
+#
+#   --source-port [!] port[:port]
+#   源端口或端口范围. 这可以是服务名称或端口号, 也可以使用格式port:port指定包含范围. 如果省略第一个端口, 则假定为"0";
+#   如果省略最后一个, 则假定为"65535". 如果第二个端口大于第一个端口, 它们将被交换. --sport是此选项别名.
+#
+#   --destination-port [!] port[:port]
+#   目标端口或端口范围. --dport是此选项别名.
+#
+#   --tcp-flags [!] mask comp
+#   匹配指定的TCP flags. 第一个参数是应该检查的标志, 写为逗号分隔列表; 第二个参数是逗号分隔的标志列表, 必须设置.
+#   flag: SYN ACK FIN RST URG PSH ALL NONE.
+#   案例:
+#   iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST SYN  // 它只匹配设置了SYN标志的数据包, 且ACK,
+#   FIN,RST未设置的数据包
+#
+#   [!] --syn
+#   匹配仅设置SYN位设置的TCP数据包, 并清除ACK,RST和FIN位. 等价于 --tcp-flags SYN,ACK,FIN,RST SYN
+#
+#   --tcp-option [!] number
+#   如果TCP选项设置, 则匹配
+#
+#   --mss value[:value]
+#   将TCP SYN或SYN/ACK数据包与指定的MSS(maximum segment size)值(或范围)匹配, 它控制该连接的最大数据包大小.
 #---------------------------------------------------------------------------------------------------
 
 
 #---------------------------------------------------------------------------------------------------
 # Target Extensions
 #
+# BALANCE
+#   这允许在给定范围的目标地址上以循环方式进行DNAT连接.
+#
+#   --to-destination ipaddr-ipaddr
+#   地址范围
+#
+# CLUSTERIP (需要测试)
+#   此模块允许配置一个简单的节点集群, 这些节点共享某个IP和MAC地址. 在此群集中的节点之间连接是静态分布.
+#
+#   --new
+#   创建一个新的ClusterIP. 必须在给定ClusterIP的第一个规则上设置此项.
+#
+#   --hashmode mode
+#   指定散列模式. 必须是sourceip, sourceip-sourceport, sourceip-sourceport-destport之一
+#
+#   --clustermac mac
+#   指定ClusterIP MAC地址. 必须是链路层多播地址.
+#
+#   --total-nodes num
+#   设置集群节点的数量
+#
+#   --local-node num
+#   设置集群的本地节点数量
+#
+#   --hash-init rnd
+#   设置hash初始化的随机种子
 #
 #
+# LOG
+#   打开匹配数据包的内核日志记录. 当为rule设置此选项时, Linux内核将通过内核日志(可以使用dmesg或syslogd读取)在所有匹
+#   配的数据包(如大多数IP头字段)上记录一些信息. 这是一个"nnon-terminating target", 即当规则不匹配时会继续使用下一个
+#   规则进行匹配. 因此, 如果要LOG拒绝的数据包, 请使用具有相同匹配条件的两个单独规则, 首先使用目标LOG, 然后使用DROP(或
+#   REJECT).
+#
+#   --log-level level
+#   设置日志级别, 查看syslog.conf
+#
+#   --log-prefix prefix
+#   设置日志前缀
+#
+#   --log-tcp-sequence
+#   记录TCP的sequence No. 有安全风险
+#
+#   --log-uid
+#   记录生成数据包的进程的uid
 #
 #
+# REDIRECT
+#   此target仅在nat表中使用. 它通过修改数据包的目标IP为incoming interface的地址从而达到数据包的重定向的目的.
+#
+#   --to-ports port-port
+#   这指定了要使用的目标端口或端口范围: 如果不指定, 则不会更改目标端口. 仅当rule指定了'-p tcp'或'-p udp'时, 此选项才
+#   有效.
 #
 #
+# REJECT
+#   用于响应匹配的数据包发回错误数据包: 它是terminating target, 会结束rule遍历. 此target仅在INPUT, FORWARD和
+# OUTPUT链以及仅从这些链调用的自定义链中有效.
+#
+#   --reject-with type
+#   设置错误数据包的类型. 包括以下类型:
+#   icmp-net-unreachable
+#   icmp-host-unreachable
+#   icmp-port-unreachable
+#   icmp-proto-unreachable
+#   icmp-net-prohibited
+#   icmp-host-prohibited or
+#   icmp-admin-prohibited (*)
+#   返回相应的ICMP错误消息(默认为icmp-port-unreachable). 使用icmp-admin-prohibited与不支持它的内核将导致一个普
+#   通的DROP而不是REJECT.
 #
 #
+# SNAT
+#   此target仅在nat表中的POSTROUTING链中有效. 它指定应修改数据包的源地址(此连接中的所有未来数据包也将被修改), 并且应
+#   停止检查规则.
+#
+#   --to-source ipaddr[-ipaddr][:port-port]
+#   它可以指定单个源IP地址或IP地址范围, 以及可选的端口范围(仅当rule还指定-p tcp或-p udp时才有效). 如果未指定端口范围,
+#   则低于512的源端口将映射到512以下的其他端口; 512和1023之间的端口将映射到1024以下的端口; 其他端口将映射到1024或更高
+#   的端口号.
+#   可以添加多个 --to-source-选项.
 #
 #
+# DNAT
+#   此target仅在nat表中的POSTROUTING链中有效. 它指定应修改数据包的目标地址(此连接中的所有未来数据包也将被修改), 并且
+#   应停止检查规则
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#   --to-destination ipaddr[-ipaddr][:port-port]
 #
 #---------------------------------------------------------------------------------------------------
