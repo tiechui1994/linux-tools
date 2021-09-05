15392b730e290a47bebc4c975ce6205ed19ec087
tiechui1994 <2904951429@qq.com>
2021-09-05 23:54:06 +0800

    add ignore files

--- develop/sync.sh
+++ develop/sync.sh
@@ -12,10 +12,16 @@ backup="~/workspace/$1"
 
 /usr/bin/inotifywait -mrq --format '%w%f' -e create,close_write,delete ${path} | while read line
 do
+    file=${path}
     if [[ -f ${line} ]]; then
-        rsync -azPv ${line} --delete ${server}:${backup}
-    else
-        cd ${path} && \
-        rsync -azPv ./ --delete ${server}:${backup}
+        file=${line}
     fi
+
+    rsync -azPv \
+        --delete \
+        --exclude="$path/.git" \
+        --exclude="$path/.idea" \
+        --exclude="$path/node_modules" \
+        --exclude="$path/public" \
+        ${file} ${server}:${backup}
 done
