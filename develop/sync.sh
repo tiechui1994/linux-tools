ab4a16291926f4b6803914b172748707372dfac4
tiechui1994 <2904951429@qq.com>
2021-09-15 23:45:41 +0800

    update sync update

--- develop/sync.sh
+++ develop/sync.sh
@@ -8,20 +8,41 @@
 
 server="user@172.16.2.170"
 path="$HOME/workspace/$1"
-backup="~/workspace/$1"
+backup="~/workspace/$2"
 
-/usr/bin/inotifywait -mrq --format '%w%f' -e create,close_write,delete ${path} | while read line
+rsync -azPv \
+    --delete \
+    --exclude="$path/.(git|git|vscode)/*" \
+    --exclude="$path/(node_modules|public)/*" \
+    ${path}/* ${server}:${backup}
+
+# modify, MODIFY sync.sh___jb_tmp___
+# attrib, ATTRIB sync.sh
+# delete, DELETE sync.sh___jb_old___  DELETE sync.sh
+# create, CREATE sync.sh___jb_tmp___  CREATE sync.sh
+ignore="^$path/(.idea|.git|.vscode|node_modules|public)"
+/usr/bin/inotifywait --quiet --recursive --monitor \
+    --format '%w%f' \
+    --event create,attrib,delete ${path} | while read line
 do
-    file=${path}
-    if [[ -f ${line} ]]; then
-        file=${line}
+    file=""
+    if [[ -f "$line" ]]; then
+        file="$line"
+    elif [[ -d "$line" ]]; then
+        file="$line"
+    else
+        file="$line"
     fi
+
+    if [[ -z ${file} || ${file} =~ ${ignore} || ${file} =~ jb_(tmp|old)___$ ]]; then
+        continue
+    fi
+    
+    echo "update: $file"
 
     rsync -azPv \
         --delete \
-        --exclude="$path/.git" \
-        --exclude="$path/.idea" \
-        --exclude="$path/node_modules" \
-        --exclude="$path/public" \
+        --exclude="$path/.(git|git|vscode)/*" \
+        --exclude="$path/(node_modules|public)/*" \
         ${file} ${server}:${backup}
 done
